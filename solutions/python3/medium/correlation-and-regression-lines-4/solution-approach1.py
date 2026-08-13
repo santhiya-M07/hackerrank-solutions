2b5d85f22b0f059faa57002c4aeeadd530f8e63c
santhiya-M07 <santhiya0722@gmail.com>
2026-08-13 09:20:54 +0530

    ✅ Correlation and Regression Lines - A Quick Recap #4 [python3] — approach1

---
+++ solutions/python3/medium/correlation-and-regression-lines-4/solution-approach1.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/correlation-and-regression-lines-4/problem?isFullScreen=true
+# Problem     Correlation and Regression Lines - A Quick Recap #4
+# Difficulty  Medium
+# Subdomain   Statistics and Machine Learning
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-13, 09:20 a.m.
+# ──────────────────────────────────────────────────
+
+y = 7
+
+x = (9 * y + 107) / 20
+
+print(f"{x:.1f}")
